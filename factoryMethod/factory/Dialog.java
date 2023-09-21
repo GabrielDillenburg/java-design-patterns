9a26ff4783e0aed4839bf1d0d39373cd5be95132
Gabriel Dillenburg <gabriel.dillenburg@ibm.com>
2023-09-21 15:11:09 -0300

    add factory method pattern example

---
+++ factoryMethod/factory/Dialog.java
@@ -0,0 +1,11 @@
+package factoryMethod.logistic;
+
+public class Logistic {
+  public void log() {
+    System.out.println("Hello from Logistic");
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Hello from main Logistic");
+  }
+}
---
+++ factoryMethod/factory/WindowsDialog.java
@@ -0,0 +1,15 @@
+package factoryMethod.factory;
+
+import factoryMethod.buttons.Button;
+import factoryMethod.buttons.WindowsButton;
+
+/**
+ * Windows Dialog will produce Windows buttons.
+ */
+public class WindowsDialog extends Dialog {
+
+    @Override
+    public Button createButton() {
+        return new WindowsButton();
+    }
+}
