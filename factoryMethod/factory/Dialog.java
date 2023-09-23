392b3c8f169fbdd1837ba03b08b6af5e1cbfed8d
Gabriel Dillenburg <gabriel.dillenburg@ibm.com>
2023-09-23 10:02:41 -0300

    fix class factories

--- factoryMethod/factory/Dialog.java
+++ factoryMethod/factory/Dialog.java
@@ -1,11 +1,24 @@
-package factoryMethod.logistic;
+package factoryMethod.factory;
 
-public class Logistic {
-  public void log() {
-    System.out.println("Hello from Logistic");
-  }
+import factoryMethod.buttons.Button;
 
-  public static void main(String[] args) {
-    System.out.println("Hello from main Logistic");
-  }
+/**
+ * Base factory class. Note that "factory" is merely a role for the class. It
+ * should have some core business logic which needs different products to be
+ * created.
+ */
+public abstract class Dialog {
+
+    public void renderWindow() {
+        // ... other code ...
+
+        Button okButton = createButton();
+        okButton.render();
+    }
+
+    /**
+     * Subclasses will override this method in order to create specific button
+     * objects.
+     */
+    public abstract Button createButton();
 }
